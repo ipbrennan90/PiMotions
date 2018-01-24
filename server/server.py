a276ba9c0ee2b5b3127ef9f0e235b258bc3c88d6
Ian Brennan <ianbrennan@ian-playbook.attlocal.net>
2018-01-23 20:07:31 -0600

    python serving react

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
-app.route("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
