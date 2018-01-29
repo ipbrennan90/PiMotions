0e65aead45c11d5c3e34965a7936705cd268387d
Ian Brennan <ianbrennan@ian-playbook.localdomain>
2018-01-29 16:29:08 -0700

    docker working, png returned from backend and displayed on fe

--- server/server.py
+++ server/server.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template, send_file, json
 import requests
+import base64
+
+# def takePicture():
+#     stream = BytesIO()
+#     camera.capture(stream, format='jpeg')
+#     stream.seek(0)
+#     im = Image.open(stream)
+#     im_buffer = im.load()
+#     return im, im_buffer
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/take')
+def take_picture():
+    image = '/justin_smells.jpeg'
+    img_str = None
+    with open(image, "rb") as imageFile:
+        img_str = base64.b64encode(imageFile.read())
+    response = Response(
+        response = json.dumps({
+            'data': 'data:image/png;base64,' + img_str
+        }),
+        status = 200,
+        mimetype='application/json'
+    )
+    return response
 
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,7 +1,35 @@
 import React, {Component} from "react"
+import axios from "axios"
+
+const imgStyle = {
+  height: "200px",
+  width: "200px",
+}
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      image: "",
+    }
+  }
+
+  handleClick() {
+    axios.get("/take").then(resp => {
+      console.log("i am the egg man")
+      this.setState({image: resp.data.data})
+    })
+  }
+
   render() {
-    return <p>Hello React!</p>
+    console.log(this.state)
+    return (
+      <div>
+        <p>This is a picture hear it roar</p>
+        <img style={imgStyle} src={this.state.image} />
+        <button onClick={this.handleClick}>BUTTON</button>
+      </div>
+    )
   }
 }
