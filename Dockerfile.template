4504a031881e2784ef158724d2bd0b825380004c
Ian Brennan <ianbrennan@ian-playbook.attlocal.net>
2018-01-31 20:02:13 -0600

    saving and reading image

---
+++ Dockerfile.template
@@ -0,0 +1,24 @@
+FROM resin/%%RESIN_MACHINE_NAME%%-python
+ENV INITSYSTEM on
+RUN apt-get update && apt-get install -y wget && rm -rf /var/lib/apt/lists/*
+# Install node
+RUN curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash - && sudo apt install nodejs
+# Install Yarn
+RUN curl -o- -L https://yarnpkg.com/install.sh | bash 
+COPY ./static/yarn.lock /static/yarn.lock
+COPY ./static/package.json /static/package.json
+COPY ./static/webpack.config.js /static/webpack.config.js
+COPY ./static/.babelrc /static/.babelrc
+WORKDIR /static
+RUN $HOME/.yarn/bin/yarn install
+COPY ./static/index.html /static/index.html
+COPY ./static/js /static/js
+COPY ./justin_smells.jpeg /justin_smells.jpeg
+RUN $HOME/.yarn/bin/yarn build
+COPY ./server/requirements.txt /server/requirements.txt
+WORKDIR /server
+ENV READTHEDOCS True
+RUN pip install -r ./requirements.txt
+COPY ./server/server.py /server/server.py
+CMD ["python", "server.py"]
+
--- server/server.py
+++ server/server.py
@@ -1,14 +1,15 @@
 from flask import Flask, Response, render_template, send_file, json
-import requests
+import picamera
+from io import BytesIO
 import base64
+import time
+from PIL import Image
+import tempfile
+import logging
+from datetime import datetime
+import os
 
-# def takePicture():
-#     stream = BytesIO()
-#     camera.capture(stream, format='jpeg')
-#     stream.seek(0)
-#     im = Image.open(stream)
-#     im_buffer = im.load()
-#     return im, im_buffer
+
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
@@ -18,18 +19,30 @@ def index():
 
 @app.route('/take')
 def take_picture():
-    image = '/justin_smells.jpeg'
-    img_str = None
-    with open(image, "rb") as imageFile:
-        img_str = base64.b64encode(imageFile.read())
-    response = Response(
-        response = json.dumps({
-            'data': 'data:image/png;base64,' + img_str
-        }),
-        status = 200,
-        mimetype='application/json'
-    )
-    return response
+    img_str = ''
+    
+    try:
+        camera = picamera.PiCamera()
+        time.sleep(2)
+        tempdir = tempfile.mkdtemp()
+        date = datetime.now()
+        filename = 'capture-{}.jpg'.format(date)
+        path = os.path.join(tempdir, filename)
+        camera.capture(path)
+        with open(path, "rb") as imageFile:
+            img_str = base64.b64encode(imageFile.read())
+    except (ValueError, RuntimeError, TypeError, NameError):
+        logging.exception("could not capture image")
+    finally:
+        response = Response(
+            response = json.dumps({
+                'data': 'data:image/jpeg;base64,' + img_str
+            }),
+            status = 200,
+            mimetype='application/json'
+        )
+        camera.close()
+        return response
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
