1abe7182a7c4c5d01063128dbe6faefc8c2b3135
Ian Brennan <ianbrennan@ian-playbook.attlocal.net>
2018-03-05 22:02:03 -0600

    clean commit

--- server/motion_detector.py
+++ server/motion_detector.py
@@ -29,71 +29,13 @@ class MotionDetector:
         self.camera.stop()
 
     def pix_diff(self, x,y, im_buff_1, im_buff_2):
-        '''
-        each pixel we grab will be an array with the RGB values of the pixel
-        represented like [R, G, B] so R is the 0th index, G is the 1st index, 
-        and B is the 2nd. The green band is the most sensitive to change. 
-        We can access the green value of each pixel at array index 1
-        '''
-        green_pixel = 1
-        '''
-        grab the green value from our pixel from the 
-        two image buffers we are comparing.
-        '''
-        green_val_1 = im_buff_1[x,y][1]
-        green_val_2 = im_buff_2[x,y][1]
-        # get the total change between the green value of the two buffers
-        pix_abs = abs(green_val_1 - green_val_2)
-        # check if it is greater than our threshold
-        if pix_abs > THRESHOLD:
-            return True
-        else:
-            return False
+        pass
         
     def check_for_motion(self, im_buffer_1, im_buffer_2):
-        motion_detected = False
-        changed_pixels = []
-        '''
-        here we are breaking down are image buffer by column and sending the x and y 
-        coordinates for each pixel to our pixel diffing function which will tell us
-        how much each pixel has changed between our two images
-        '''
-        for x in xrange(0, self.camera.width):
-            # going over each y value in our column
-            for y in range(0, self.camera.height):
-                # checking if the pixel has changed with our pixel diffing function
-                pixel_changed = self.pix_diff(x,y,im_buffer_1, im_buffer_2)
-                # adding a True or False value that was returned from pix_diff
-                changed_pixels.append(pixel_changed)
-        # this gives us a sum of the true values in our changed_pixels array
-        total_changed_pixels = sum(changed_pixels)
-        # check if the number of changed pixels is greater than our sensitivity
-        if total_changed_pixels > SENSITIVITY:
-            return True, total_changed_pixels
-        else:
-            return False, total_changed_pixels
+        pass
         
     def detector(self):
-        # capture our first image
-        im_1_buffer = self.camera.capture_image()
-        # this starts a while loop
-        while True:
-            # here we are checking if we still want to be running the detector
-            if not RUN_DETECTOR:
-                '''
-                if we don't want to be running the detector we stop and break out of the loop
-                this also exits the thread we started
-                '''
-                self.stop()
-                break
-            # if the loop is running we capture our second image
-            im_2_buffer = self.camera.capture_image()
-            # check for motion
-            motionDetected, pixChanges = self.check_for_motion(im_1_buffer, im_2_buffer)
-            # send the results to our callback
-            self.cb(pixChanges, motionDetected)
-            # set our last image as the next to be checked against for motion
-            im_1_buffer = im_2_buffer
+        pass
 
     
 
